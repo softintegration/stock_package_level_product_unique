4db0726feb3f558e3949b3f2dbbbf6d58ad7339f
Mossaab BOUDCHICHA <boudchicha23@hotmail.fr>
2023-01-05 14:23:32 +0100

    Package level:Add field <Product> in the tree view

--- models/stock_package_level.py
+++ models/stock_package_level.py
@@ -8,6 +8,6 @@ class StockPackageLevel(models.Model):
     _inherit = 'stock.package_level'
 
     global_quantity = fields.Float(string='Global quantity',related='package_id.global_quantity',store=True)
-
+    product_id = fields.Many2one('product.product', string='Product', related='package_id.product_id', store=True)
     
 
